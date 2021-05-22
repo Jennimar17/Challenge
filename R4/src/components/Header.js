147504be4156d265d214b2a1dc5584325d516930
Jeniffer Hurtado <62664863+Jennimar17@users.noreply.github.com>
2021-05-21 21:10:30 -0300

    agrego boton de buscar, alerta de validacion de campo vacio

--- R4/src/components/Header.js
+++ R4/src/components/Header.js
@@ -1,13 +1,63 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./Header.css";
 
+
 const Header = ({ titulo }) => {
+    //statee
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: ''
+    });
+     const [error, guardarError] = useState(false);
+
+
+    //extraer ciudad
+    const { ciudad } = busqueda;
+
+    //funcion coloca elements en el state
+    const handleChange = e => {
+        //actualiza el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+    //cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //para validar
+            if(ciudad.trim() === '') {
+                guardarError(true);
+                return;
+            }
+
+            guardarError(false);
+
+        //pasarlo al comp principal
+    }
+
   return (
     <Fragment>
       <h1>{titulo}</h1>
-      <form className='form-style'>
-        <input type="text" name="ciudad" id="ciudad" />
-        <label>Buscar Ciudad</label>
+      <form 
+            onSubmit={handleSubmit}
+            className='form-style'
+      >
+    {error ? <span className='alert'>¡Ingresa una Ciudad!</span> : null }
+        <input 
+            type="text" 
+            name="ciudad" 
+            id="ciudad"  
+            value={ciudad}
+            onChange={handleChange}
+        />
+        
+
+        <input 
+             type='submit'
+             value='Buscar Ciudad'
+             className='button-style'
+        />  
       </form>
     </Fragment>
   );
